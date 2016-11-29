b01a6e464c4e5b2e4dba180e04692b86e3d03625
Tyr <tyr@discowolf.local>
2016-11-28 22:50:11 -0800

    device manager

--- main.py
+++ main.py
@@ -11,11 +11,9 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(levelname)8s (%(threadName)-10s) %(message)s',
                     )
 
-# logging.basicConfig(level=logging.DEBUG)
-
 logging.info("starting")
 
-devices = {}
+app_dir = os.path.split(os.path.abspath(__file__))[0]
 
 
 # Utility Functions
@@ -36,7 +34,7 @@ def map_int(v, fl, fh, tl, th):
 
 # Base Device Object
 class DeviceObj:
-    def __init__(self, name, t, x=1, y=1):
+    def __init__(self, manager, name, t, x=1, y=1):
         # Save Parameters
         self.sizeX = x
         self.sizeY = y
@@ -48,7 +46,7 @@ class DeviceObj:
         self._showBuffer = make_color_grid(self.sizeX, self.sizeY)
 
         # Store in Devices dictionary
-        devices[self.name] = self
+        manager.add_device(self)
 
         logging.debug("created device [{0} ({1}, {2})]".format(self.name, self.sizeX, self.sizeY))
 
@@ -75,9 +73,9 @@ class DeviceObj:
 
 # UnicornHat Object
 class UnicornHat(DeviceObj):
-    def __init__(self, name, rot=0, bri=1):
+    def __init__(self, manager, name, rot=0, bri=1):
         width, height = unicorn.get_shape()
-        DeviceObj.__init__(self, name, "unicornhat", width, height)
+        DeviceObj.__init__(self, manager, name, "unicornhat", width, height)
 
         # Save Parameters
         self.rotation = rot
@@ -85,8 +83,8 @@ class UnicornHat(DeviceObj):
 
         # Initialize Unicorn Hat
         unicorn.set_layout(unicorn.HAT)
-        unicorn.rotation(rotation)
-        unicorn.brightness(brightness)
+        unicorn.rotation(rot)
+        unicorn.brightness(bri)
 
     def set(self, r, g, b, x=0, y=0):
         DeviceObj.set(self, r, g, b, x, y)
@@ -99,6 +97,61 @@ class UnicornHat(DeviceObj):
 
         # Update Display
         unicorn.show()
+
+
+class DevicePosition:
+    def __init__(self, name, x, y, w, h):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def is_inside(self, x, y):
+        return (self.x <= x < self.x + self.w) and (self.y <= y < self.y + self.h)
+
+    def translate(self, x, y):
+        new_x = x - self.x
+        new_y = y - self.y
+
+        return [new_x, new_y]
+
+
+class DeviceManager:
+    def __init__(self):
+        self.devices = {}
+        self._devicesLock = threading.Lock()
+
+        # Build Devices
+        device_config = configparser.ConfigParser()
+        devices_file = os.path.join(app_dir, 'config', 'devices.ini')
+        device_config.read(devices_file)
+
+        for d in device_config.sections():
+            if 'type' in device_config[d]:
+                if device_config[d]['type'] == 'unicornhat':
+                    if 'rotation' in device_config[d]:
+                        rotation = int(device_config[d]['rotation'])
+                    else:
+                        rotation = 0
+
+                    if 'brightness' in device_config[d]:
+                        brightness = float(device_config[d]['brightness'])
+                    else:
+                        brightness = 1
+
+                    logging.debug(
+                        "attempting to init Unicorn Hat [{0} bri:{1} rot:{2}]".format(d, brightness, rotation))
+                    UnicornHat(self, d, rotation, brightness)
+                else:
+                    logging.warning(
+                        "device {0} has unsupported type: {1}. ignoring.".format(d, device_config[d]['type']))
+            else:
+                logging.warning("device {0} doesn't have a type. ignoring.".format(d))
+
+    def add_device(self, d):
+        with self._devicesLock:
+            self.devices[d.name] = d
 
 
 # Thread Safe Buffer for Frames
@@ -130,17 +183,17 @@ class FrameBuffer:
 def thread_frame_maker(nf):
     logging.info("starting FrameMaker")
     time.sleep(2)
-    nf.set(make_color_grid(8, 8, 127))
+    nf.set(make_color_grid(8, 8, 255))
 
 
 # LightWrite
 # Compares new frame buffer to current buffer and updates lights
-def thread_light_write(cf, nf):
+def thread_light_write(cf, nf, dm):
     logging.info("starting LightWrite")
 
     # List configured devices
-    for name in devices:
-        logging.info("found device [{0}]".format(devices[name], ))
+    for name in dm.devices:
+        logging.info("found device [{0}]".format(dm.devices[name], ))
 
     while True:
         current_frame = cf.get()
@@ -157,40 +210,16 @@ def thread_light_write(cf, nf):
                         g = next_frame[x][y][1]
                         b = next_frame[x][y][2]
 
-                        devices["test"].set(r, g, b, x, y)
+                        dm.devices["test"].set(r, g, b, x, y)
 
-            devices["test"].show()
+            dm.devices["test"].show()
 
             cf.set(next_frame)
 
 
 if __name__ == "__main__":
-    app_dir = os.path.split(os.path.abspath(__file__))[0]
-
-    # Build Devices
-    device_config = configparser.ConfigParser()
-    devices_file = os.path.join(app_dir, 'config', 'devices.ini')
-    device_config.read(devices_file)
-
-    for d in device_config.sections():
-        if 'type' in device_config[d]:
-            if device_config[d]['type'] == 'unicornhat':
-                if 'rotation' in device_config[d]:
-                    rotation = int(device_config[d]['rotation'])
-                else:
-                    rotation = 0
-
-                if 'brightness' in device_config[d]:
-                    brightness = float(device_config[d]['brightness'])
-                else:
-                    brightness = 1
-
-                logging.debug("attempting to init Unicorn Hat [{0} bri:{1} rot:{2}]".format(d, rotation, brightness))
-                UnicornHat(d, rotation, brightness)
-            else:
-                logging.warning("device {0} has unsupported type: {1}. ignoring.".format(d, device_config[d]['type']))
-        else:
-            logging.warning("device {0} doesn't have a type. ignoring.".format(d))
+    # Initialize Devices
+    device_manager = DeviceManager()
 
     # Frame Buffer to pass frames between threads
     current_frame_buffer = FrameBuffer()
@@ -198,7 +227,7 @@ if __name__ == "__main__":
 
     tlw = threading.Thread(name='LightWrite',
                            target=thread_light_write,
-                           args=(current_frame_buffer, next_frame_buffer))
+                           args=(current_frame_buffer, next_frame_buffer, device_manager))
     tlw.start()
 
     tfm = threading.Thread(name='FrameMaker',
